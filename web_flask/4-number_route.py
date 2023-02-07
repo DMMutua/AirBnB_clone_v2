a8d157e2ba38b3a0f5efe849c28a77bea125140a
DMMutua <lengowriter99@gmail.com>
2023-02-07 12:25:37 +0300

    using flask.Flask.app.route() functionality to check whether param passed to URL is digit or not

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""A Script that Starts a Flask Web Application dubbed `app` with 5 Views:
+        the Index View <static><text>
+        the /hbnb View <static><text>
+        the /c/<text> View <dynamic><text>
+        the /python/<text> View <dynamic><text>
+        the /number/<n> View <dynamic><number>
+
+   The `app` listens on; 0.0.0.0:, port 5000.
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def homepage():
+    """A Function that Displays `app`'s Index View
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A Function that Returns the word 'HBNB' when Executed"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cplus(text):
+    """Function that returns a string 'C' followed by contents of `text`
+    All Underscores on the variable `text` are replaced by space symbol.
+    """
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonplus(text='is cool'):
+    """Function that returns a string: 'Python ' followed by contents of `text`
+    All Underscores in `text` are replaced by space symbol.
+    The default value for text is "is cool".
+    """
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isitnum(n):
+    """Displays the text; `<n> is a number` only if <n> is a number
+    Otherwise returns nothing; which evaluates a 404 error on browser"""
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
