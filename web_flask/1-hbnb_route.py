6e4d89230a51edfb4819337f95a3d733df974c78
DMMutua <lengowriter99@gmail.com>
2023-02-07 10:27:04 +0300

    Adding another view to `app`

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""A Script that Starts a Flask Web Application with 1 View - the Index View
+"""A Script that Starts a Flask Web Application with 2 Views:
+        the Index View
+        the /hbnb View
 """
 
 
@@ -14,6 +16,12 @@ def homepage():
     """A Function that Displays `app`'s Index View
     """
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A Function that Returns the word 'HBNB' when Executed"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
---
+++ web_flask/__init__.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+"""Makes web_flask an Importable Module"""
